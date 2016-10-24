e40d09fd17af4782bef6405a4831bca1d8862156
barrett <barrett.tucker@gmail.com>
2016-10-24 13:46:38 -0700

    defaulting thymeleaf json message source properties

--- openid-connect-server-spring-boot-ui-thymeleaf/src/main/java/org/mitre/springboot/config/ui/WebMvcConfig.java
+++ openid-connect-server-spring-boot-ui-thymeleaf/src/main/java/org/mitre/springboot/config/ui/WebMvcConfig.java
@@ -3,9 +3,12 @@ package org.mitre.springboot.config.ui;
 import org.mitre.openid.connect.config.JsonMessageSource;
 import org.mitre.openid.connect.web.ServerConfigInterceptor;
 import org.mitre.openid.connect.web.UserInfoInterceptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -17,36 +20,44 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
 	@Bean
+	@ConditionalOnMissingBean(UserInfoInterceptor.class)
 	public UserInfoInterceptor getUserInfoInterceptor() {
 		return new UserInfoInterceptor();
 	}
-	
+
 	@Bean
+	@ConditionalOnMissingBean(ServerConfigInterceptor.class)
 	public ServerConfigInterceptor getServerConfigInterceptor() {
 		return new ServerConfigInterceptor();
 	}
-	
+
 	@Bean
-	@ConfigurationProperties(prefix="openid.connect.jsonMessageSource")
-	public JsonMessageSource messageSource() {
-		return new JsonMessageSource();
+	@ConditionalOnMissingBean(JsonMessageSource.class)
+	@ConfigurationProperties(prefix = "openid.connect.jsonMessageSource")
+	public JsonMessageSource messageSource(
+			@Value("classpath:/static/resources/js/locale/") Resource baseDirectory,
+			@Value("true") Boolean useCodeAsDefaultMessage) {
+		JsonMessageSource jsonMessageSource = new JsonMessageSource();
+		jsonMessageSource.setBaseDirectory(baseDirectory);
+		jsonMessageSource.setUseCodeAsDefaultMessage(useCodeAsDefaultMessage);
+		return jsonMessageSource;
 	}
-	
+
 	@Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(getUserInfoInterceptor());
-        registry.addInterceptor(getServerConfigInterceptor());
-    }
-	
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(getUserInfoInterceptor());
+		registry.addInterceptor(getServerConfigInterceptor());
+	}
+
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/login").setViewName("login");
 	}
-	
+
 	@Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**").addResourceLocations("classpath:/static/resources/");
-    }
-	
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations(
+				"classpath:/static/resources/");
+	}
 
 }
