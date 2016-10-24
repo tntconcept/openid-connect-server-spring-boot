b7b1a2af8220e535db14fbea9da7827b06b9c09a
barrett <barrett.tucker@gmail.com>
2016-10-24 14:34:08 -0700

    ensuring config context load test starts container

--- openid-connect-server-spring-boot-config/src/test/java/org/mitre/boot/security/TestBootApplicationTests.java
+++ openid-connect-server-spring-boot-config/src/test/java/org/mitre/boot/security/TestBootApplicationTests.java
@@ -1,16 +1,38 @@
 package org.mitre.boot.security;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = TestBootApplication.class)
 @WebAppConfiguration
 public class TestBootApplicationTests {
 
+	@Autowired
+	protected WebApplicationContext context;
+
+	@Autowired
+	protected FilterChainProxy springSecurityFilterChain;
+	
+	protected MockMvc mockMvc;
+
+	@Before
+	public void setUp() {
+		this.mockMvc = MockMvcBuilders
+			.webAppContextSetup(this.context)
+			.addFilter(springSecurityFilterChain)
+			.build();
+	}
+	
 	@Test
 	public void contextLoads() {
 	}
