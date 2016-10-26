ebe451e79d8c7b84facf3718a8a8777e9f51b034
barrett <barrett.tucker@gmail.com>
2016-10-26 11:47:16 -0700

    turning of signing for snapshots during travis testing

---
+++ publish.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+./gradlew uploadArchives -i -s
+RETVAL=$?
+
+if [ $RETVAL -eq 0 ]; then
+    echo 'Artifacts published to Sonatype'
+else
+    echo 'Sonatype publish failed.'
+    return 1
+fi
