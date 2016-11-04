0c7dfcbeb9dc707b255ce9aa92b5352c4aece89b
barrett <barrett.tucker@gmail.com>
2016-11-04 13:35:32 -0700

    making @EnableGlobalAuthentication more explicit

--- openid-connect-server-spring-boot-config/src/main/java/org/mitre/springboot/config/JdbcDataSourceAuthenticationConfig.java
+++ openid-connect-server-spring-boot-config/src/main/java/org/mitre/springboot/config/JdbcDataSourceAuthenticationConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
@@ -15,6 +16,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  */
 @Order(610)
 @Configuration
+@EnableGlobalAuthentication
 public class JdbcDataSourceAuthenticationConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
