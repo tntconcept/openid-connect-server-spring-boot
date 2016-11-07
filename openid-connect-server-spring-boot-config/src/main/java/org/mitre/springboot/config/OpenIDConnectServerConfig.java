ca7f18e66012c6426d8d7be29c05c7632e6bb298
Nate Grover <nate@simple-dynamics.com>
2016-11-07 10:52:22 -0800

    Configuration for enabling/disabling ClientAPI endpoint.

--- openid-connect-server-spring-boot-config/src/main/java/org/mitre/springboot/config/OpenIDConnectServerConfig.java
+++ openid-connect-server-spring-boot-config/src/main/java/org/mitre/springboot/config/OpenIDConnectServerConfig.java
@@ -3,6 +3,7 @@ package org.mitre.springboot.config;
 import org.mitre.openid.connect.config.ConfigurationPropertiesBean;
 import org.mitre.openid.connect.web.ApprovedSiteAPI;
 import org.mitre.openid.connect.web.BlacklistAPI;
+import org.mitre.openid.connect.web.ClientAPI;
 import org.mitre.openid.connect.web.RootController;
 import org.mitre.openid.connect.web.WhitelistAPI;
 import org.mitre.springboot.config.annotation.EnableOpenIDConnectServer;
@@ -133,7 +134,7 @@ public class OpenIDConnectServerConfig {
 	 * Separate config for this package with excludes for each of the specific class configurations that follow.  RootController will remain excluded by default and is part of the UI project only. 
 	 */
 	@Configuration
-	@ComponentScan(basePackages={"org.mitre.openid.connect.web"}, excludeFilters = @Filter(type = FilterType.ASSIGNABLE_TYPE, classes={WhitelistAPI.class, RootController.class, ApprovedSiteAPI.class, BlacklistAPI.class}))
+	@ComponentScan(basePackages={"org.mitre.openid.connect.web"}, excludeFilters = @Filter(type = FilterType.ASSIGNABLE_TYPE, classes={WhitelistAPI.class, RootController.class, ApprovedSiteAPI.class, BlacklistAPI.class, ClientAPI.class}))
 	public static class WebEndpointConfiguration {}
 
 	@Configuration
@@ -150,5 +151,10 @@ public class OpenIDConnectServerConfig {
 	@ConditionalOnProperty(havingValue="true", name="openid.connect.endpoints.blacklist.enabled", matchIfMissing=true)
 	@Import(value=BlacklistAPI.class)
 	public static class BlacklistEndpointConfiguration {}
+
+	@Configuration
+	@ConditionalOnProperty(havingValue="true", name="openid.connect.endpoints.client.enabled", matchIfMissing=true)
+	@Import(value=ClientAPI.class)
+	public static class ClientEndpointConfiguration {}
 
 }
