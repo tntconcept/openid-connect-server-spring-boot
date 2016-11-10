344fc819db90630040974a0774862d9406e9a39b
barrett <barrett.tucker@gmail.com>
2016-11-09 16:09:58 -0800

    test improvement to token api tests

--- openid-connect-server-spring-boot-test/src/test/java/org/mitre/springboot/oauth2/web/OAuthConfirmationControllerAuthorizationTests.java
+++ openid-connect-server-spring-boot-test/src/test/java/org/mitre/springboot/oauth2/web/OAuthConfirmationControllerAuthorizationTests.java
@@ -11,4 +11,5 @@ public class OAuthConfirmationControllerAuthorizationTests extends ApiAuthorizat
 	//TODO OauthConfirmationControllerAuthorizationTests
 	@Test
 	public void TODO(){}
+	
 }
--- openid-connect-server-spring-boot-test/src/test/java/org/mitre/springboot/oauth2/web/TokenApiAuthorizationTests.java
+++ openid-connect-server-spring-boot-test/src/test/java/org/mitre/springboot/oauth2/web/TokenApiAuthorizationTests.java
@@ -3,11 +3,17 @@ package org.mitre.springboot.oauth2.web;
 import javax.transaction.Transactional;
 
 import org.junit.Test;
-import org.mitre.springboot.EndpointTestsBase;
+import org.mitre.springboot.openid.connect.web.ApiAuthorizationTestsBase;
 
 @Transactional
-public class TokenApiAuthorizationTests extends EndpointTestsBase {
+public class TokenApiAuthorizationTests extends ApiAuthorizationTestsBase {
 
+	@Test
+	public void adminGetApiTokenSuccess() throws Exception {
+		adminSession();
+		checkGetAccess("/api/tokens/access", 200);
+	}
+	
 	//TODO token tests for the token API endpoints
 	@Test
 	public void TODO(){}
