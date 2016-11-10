9f3991f00eed23201d483d5675ef4a8d41e23322
barrett <barrett.tucker@gmail.com>
2016-11-10 14:35:24 -0800

    unused scan clean up

--- openid-connect-server-spring-boot-config/src/main/java/org/mitre/springboot/config/OpenIDConnectServerConfig.java
+++ openid-connect-server-spring-boot-config/src/main/java/org/mitre/springboot/config/OpenIDConnectServerConfig.java
@@ -130,56 +130,7 @@ import org.springframework.web.servlet.view.BeanNameViewResolver;
 
 @Configuration
 @ConditionalOnClass(EnableOpenIDConnectServer.class)
-@ComponentScan(basePackages = {
-		
-//"org.mitre", 
-
-//Server packages
-//"org.mitre.discovery.view",
-//"org.mitre.discovery.web",
-//"org.mitre.oauth2.repository.impl",
-//"org.mitre.oauth2.service.impl",
-//"org.mitre.oauth2.token",
-//"org.mitre.oauth2.view",
-//"org.mitre.oauth2.web",
-//"org.mitre.openid.connect.assertion",
-//"org.mitre.openid.connect.config",
-//"org.mitre.openid.connect.exception",
-//"org.mitre.openid.connect.filter",
-//"org.mitre.openid.connect.repository.impl",
-//"org.mitre.openid.connect.request",
-//"org.mitre.openid.connect.service.impl",
-//"org.mitre.openid.connect.token",
-//"org.mitre.openid.connect.util",
-//"org.mitre.openid.connect.view",
-//"org.mitre.openid.connect.web",
-
-//Commons packages
-//"org.mitre.discovery.util",
-//"org.mitre.jose.keystore",
-//"org.mitre.jwt.encryption.service",
-//"org.mitre.jwt.encryption.service.impl",
-//"org.mitre.jwt.signer.service",
-//"org.mitre.jwt.signer.service.impl",
-//"org.mitre.oauth2.model",
-//"org.mitre.oauth2.model.convert",
-//"org.mitre.oauth2.repository",
-//"org.mitre.oauth2.service",
-//"org.mitre.openid.connect",
-//"org.mitre.openid.connect.model",
-//"org.mitre.openid.connect.model.convert",
-//"org.mitre.openid.connect.repository",
-//"org.mitre.openid.connect.service",
-//"org.mitre.uma.model",
-//"org.mitre.uma.model.convert",
-//"org.mitre.uma.repository",
-//"org.mitre.uma.service",
-//"org.mitre.util",
-//"org.mitre.util.jpa",
-
-//Spring boot packages
-"org.mitre.springboot.config"}
-)
+@ComponentScan(basePackages = {"org.mitre.springboot.config"})
 @EnableWebSecurity
 @EnableResourceServer
 @EnableAuthorizationServer
